47880c2c4bd4dde3a2f358690f3b20a05d37e2f0
SiddharthKorukonda <siddharth.korukonda.sk@gmail.com>
2026-02-17 13:31:28 -0500

    chore: updated the resume page with new resume from viewable

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -5,7 +5,7 @@ import { ResumeIcon, ExternalLinkIcon } from '../components/icons'
 
 // Path to your resume PDF in the public folder
 // Make sure the filename matches exactly what's in your public folder
-const RESUME_PATH = './Resume - 1_28_26.pdf'
+const RESUME_PATH = './Resume - 2_17_26.pdf'
 
 export default function Resume() {
   const { isRetro, isDark } = useTheme()
