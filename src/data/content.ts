74c292de7b9053529c4948c48c18c120fa76379e
SiddharthKorukonda <siddharth.korukonda.sk@gmail.com>
2026-02-23 19:47:24 -0500

    chore: added total aime + amc score for aime distinction

--- src/data/content.ts
+++ src/data/content.ts
@@ -299,7 +299,7 @@ export const awards: Award[] = [
     id: "award-aime-2022",
     name: "American Invitational Mathematics Examination",
     organization: "The Bronx High School of Science",
-    description: "• AIME - 5\n• AMC 12B - 106.5",
+    description: "• AIME - 5\n• AMC 12B - 106.5\n• Total - 156.5",
     date: "February 2022",
     sortOrder: 202202,
   },
